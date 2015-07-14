d48b121fe7f57e9fd282bebea002c2aaf6d4d75b
caspervdw <caspervdw@gmail.com>
2015-07-14 10:04:24 +0200

    BLD add manifest
    So that ND2SDK and demo file are copied during setup.py
    - add OS requirement
    - add version 0.1

--- setup.py
+++ setup.py
@@ -8,12 +8,20 @@ def read(fname):
 
 setup_parameters = dict(
     name="PIMS_ND2",
+    version="0.1",
     description="ND2SDK wrapper for PIMS",
     author="Casper van der Wel",
     install_requires=['pims>=0.3'],
     author_email="caspervdw@gmail.com",
-    url="https://github.com/caspervdw/pims_nd2",
+    url="https://github.com/soft-matter/pims_nd2",
     packages=['pims_nd2'],
+    include_package_data=True,
+    platforms=[
+#        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: Microsoft :: Windows :: Windows Vista',
+#        'Operating System :: POSIX :: Linux',
+    ],
     long_description=read('README.md'))
 
 setup(**setup_parameters)
