417b51b2b04eb6a475d0e8f96183b16c72007405
stevendbrown <sdbrown@gmail.com>
2015-11-05 14:26:17 -0800

    Added LoadLibrary lines for OS X & updated ND2SDK libraries.

--- pims_nd2/ND2SDK.py
+++ pims_nd2/ND2SDK.py
@@ -8,8 +8,9 @@ if platform == "linux" or platform == "linux2":
     nd2 = cdll.LoadLibrary(os.path.join(os.path.dirname(__file__), 'ND2SDK',
                                         'linux', 'libnd2ReadSDK.so'))
 elif platform == "darwin":
-   raise OSError("Unsupported OS. The ND2SDK for OSX is included, "
-                 "please try to implement!")
+    dlldir = os.path.join(os.path.dirname(__file__), 'ND2SDK', 'osx', 'nd2sdk.framework', 'versions', '1')
+    dllname = "nd2sdk"
+    nd2 = cdll.LoadLibrary(dlldir + "/" + dllname)
 elif platform == "win32":
     bitsize = sizeof(c_void_p) * 8
     if bitsize == 32:
